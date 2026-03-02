3072e27bf6e58dd99c8779d24a8b423d574fd20a
erdincpulat-del <erdincpulat@gmail.com>
2026-03-02 21:38:09 +0300

    Update lib:supabaseServer.ts

--- app/lib:supabaseServer.ts
+++ app/lib:supabaseServer.ts
@@ -1,7 +1,10 @@
-import { supabaseServer } from "@/app/lib/supabaseServer";
+import { createClient } from "@supabase/supabase-js";
 
 export function supabaseServer() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
   const key = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-  return createClient(url, key, { auth: { persistSession: false } });
+
+  return createClient(url, key, {
+    auth: { persistSession: false },
+  });
 }
